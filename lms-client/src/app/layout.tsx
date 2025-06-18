9dbe0e93f2c55f45c63a74e883bd6e1efd18bcf2
Laxmi-999 <regmilaxmi325@gmail.com>
2025-06-18 09:05:37 +0545

    moved redux to src folder

--- lms-client/src/app/layout.tsx
+++ lms-client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ReduxProvider from './Redux/ReduxProvider'
 import { Toaster } from "@/components/ui/sonner"
+import ReduxProvider from '../Redux/ReduxProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
